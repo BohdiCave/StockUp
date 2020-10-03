f0c74f97f49dc866936add5dfbfbe07304ddaeca
David Fischer <dfischer@fischerltd.com>
2020-10-03 11:32:08 -0400

    articles 001

---
+++ df-script.js
@@ -0,0 +1,27 @@
+  var settings = {
+	"async": true, 
+	"crossDomain": true,
+	"url": "https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete?region=US&q=tesla",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+		"x-rapidapi-key": "dc4dec46cdmshbe33f33e8f04383p1540fbjsnd5fecd94ffc8"
+	}
+}
+
+$.ajax(settings).then(function (response) {
+	
+	
+	for (var i=0; i< response.news.length; i++){
+		var articleRow = $(".news-item"+i);
+		var aRow = $("<tr>");
+		var titleTd = $("<td>").text(response.news[i].title);
+		var linkTd = $("<td>").text(response.news[i].link);
+		// var articleLink = '<a href="' + response.news[i].link + '"></a>'
+		// var linkTd = $("<td>").text(articleLink);
+		var publisherTd = $("<td>").text(response.news[i].publisher);
+		aRow.append(publisherTd, titleTd, linkTd);
+		articleRow.append(aRow);
+	}
+	articleRow.append(aRow);
+});
